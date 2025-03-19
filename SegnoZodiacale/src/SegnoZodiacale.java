158601d00ad0a4572bffc43aded11d4c938c4f8d
AndreaGregori-18 <gregori.andrea05@gmail.com>
2025-03-19 12:25:53 +0100

    modifica2

--- SegnoZodiacale/src/SegnoZodiacale.java
+++ SegnoZodiacale/src/SegnoZodiacale.java
@@ -27,5 +27,48 @@ public class SegnoZodiacale {
             default:
                 System.out.println("Mese o giorno inserito non valido");
         }
+
     }
+
+    public void segniZodiacali() {
+
+        if(mese.equals("dicembre") && giorno >= 22 && giorno <= 31 || mese.equals("gennaio") && giorno >= 1 && giorno <= 19) {
+            System.out.println("Capricorno");
+
+        } else if(mese.equals("gennaio") && giorno >= 20 && giorno <= 31 || mese.equals("febbraio") && giorno >= 1 && giorno <= 18) {
+            System.out.println("Acquario");
+
+        } else if(mese.equals("febbraio") && giorno >= 19 && giorno <= 28 || mese.equals("marzo") && giorno >= 1 && giorno <= 20) {
+            System.out.println("Pesci");
+
+        } else if(mese.equals("marzo") && giorno >= 21 && giorno <= 31 || mese.equals("aprile") && giorno >= 1 && giorno <= 19) {
+            System.out.println("Ariete");
+
+        } else if(mese.equals("aprile") && giorno >= 20 && giorno <= 30 || mese.equals("maggio") && giorno >= 1 && giorno <= 20) {
+            System.out.println("Toro");
+
+        } else if(mese.equals("maggio") && giorno >= 21 && giorno <= 31 || mese.equals("giugno") && giorno >= 1 && giorno <= 20) {
+            System.out.println("Gemelli");
+
+        } else if(mese.equals("giugno") && giorno >= 21 && giorno <= 30 || mese.equals("luglio") && giorno >= 1 && giorno <= 22) {
+            System.out.println("Cancro");
+
+        } else if(mese.equals("luglio") && giorno >= 23 && giorno <= 31 || mese.equals("agosto") && giorno >= 1 && giorno <= 22) {
+            System.out.println("Leone");
+
+        } else if(mese.equals("agosto") && giorno >= 23 && giorno <= 31 || mese.equals("settembre") && giorno >= 1 && giorno <= 22) {
+            System.out.println("Vergine");
+
+        } else if(mese.equals("settembre") && giorno >= 23 && giorno <= 30 || mese.equals("ottobre") && giorno >= 1 && giorno <= 22) {
+            System.out.println("Bilancia");
+
+        } else if(mese.equals("ottobre") && giorno >= 23 && giorno <= 31 || mese.equals("novembre") && giorno >= 1 && giorno <= 21) {
+            System.out.println("Scorpione");
+
+        } else if(mese.equals("novembre") && giorno >= 22 && giorno <= 30 || mese.equals("dicembre") && giorno >= 1 && giorno <= 21) {
+            System.out.println("Sagittario");
+
+        }
+    }
+
 }
--- SegnoZodiacale/src/SegnoZodiacaleTest.java
+++ SegnoZodiacale/src/SegnoZodiacaleTest.java
@@ -1,3 +1,11 @@
 public class SegnoZodiacaleTest {
     
+    public static void main(String[] args) {
+        
+        SegnoZodiacale mioSegnoZodiacale = new SegnoZodiacale();
+        mioSegnoZodiacale.mese = "febbraio";
+        mioSegnoZodiacale.giorno = 29;
+        mioSegnoZodiacale.mesi();
+        mioSegnoZodiacale.segniZodiacali();
+    }
 }
